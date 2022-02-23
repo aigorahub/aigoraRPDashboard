ba417284be55a957d79fd764f3338268e7c8dd79
Bartosz Smulski <bartosz.smulski@aigora.com>
2022-02-23 16:24:48 +0100

    added documentation of sumtable function

--- R/make_sumtable.R
+++ R/make_sumtable.R
@@ -1,3 +1,32 @@
+
+#' Sumtable
+#'
+#' @param dataio input dataframe
+#' @param .products_colname products column
+#' @param .attributes_colname attribute names column
+#' @param .attribute_value_colname attribute values column
+#' @param .id_cols id columns
+#' @param selected_products vector of product names to be selected
+#' @param selected_attributes vector of attributes names to be selected
+#'
+#' @return c("tbl_df", "tbl", "data.frame")
+#' @export
+#'
+#' @examples
+#' data <- mtcars
+#' data$product <- rownames(data)
+#'
+#' data <- data %>%
+#'   tidyr::pivot_longer(cols = -c(product, cyl))
+#'
+#' data_transformed <- make_sumtable(data = data,
+#'                                   .products_colname = product,
+#'                                   .attributes_colname = name,
+#'                                   .attribute_value_colname = value,
+#'                                   .id_cols = -value,
+#'                                   selected_products = c("Merc 280", "Fiat 128", "Mazda RX4"),
+#'                                   selected_attributes = c("mpg", "disp"))
+#'
 make_sumtable <- function(data,
                           .products_colname,
                           .attributes_colname,
